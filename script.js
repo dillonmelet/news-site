68d85133e640dd028082190c6386092659745fd1
dillonmelet <75828346+dillonmelet@users.noreply.github.com>
2021-03-27 00:14:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,154 @@
-function show1() {
-  document.getElementById("div1").style.display = "none";
+"use strict";
+
+function question1() {
+  var x = document.getElementById("answer1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
-function show2() {
-  document.getElementById("div1").style.display = "block";
+
+function question2() {
+  var x = document.getElementById("answer2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function question3() {
+  var x = document.getElementById("answer3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function question4() {
+  var x = document.getElementById("answer4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function explain1() {
+  var x = document.getElementById("explanation1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function explain2() {
+  var x = document.getElementById("explanation2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function explain3() {
+  var x = document.getElementById("explanation3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function explain4() {
+  var x = document.getElementById("explanation4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function explain5() {
+  var x = document.getElementById("explanation5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function explain6() {
+  var x = document.getElementById("explanation6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function explain7() {
+  var x = document.getElementById("explanation7");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function explain8() {
+  var x = document.getElementById("explanation8");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function explain9() {
+  var x = document.getElementById("explanation9");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function explain10() {
+  var x = document.getElementById("explanation10");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function explain11() {
+  var x = document.getElementById("explanation11");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function explain12() {
+  var x = document.getElementById("explanation12");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function explain13() {
+  var x = document.getElementById("explanation13");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
